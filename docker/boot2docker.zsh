857edf8ca78b85b8e182229808ce90914718cd8a
Ingo Claro <ingo@groupon.com>
2014-11-18 12:14:23 -0800

    add boot2docker env variables with a hostname

--- docker/boot2docker.zsh
+++ docker/boot2docker.zsh
@@ -2,3 +2,8 @@
 docker-ip() {
   boot2docker ip 2> /dev/null
 }
+
+export DOCKER_TLS_VERIFY=1
+export DOCKER_HOST="tcp://boot2docker:2376"
+export DOCKER_CERT_PATH=~/.boot2docker/certs/boot2docker-vm
+
