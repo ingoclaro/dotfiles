10d501335d67466dc48b8dcc85d313f993e73140
Ingo Claro <ingo@groupon.com>
2018-01-14 15:22:47 -0300

    updates

--- java/env.zsh
+++ java/env.zsh
@@ -3,3 +3,5 @@ export MAVEN_OPTS="-Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m"
 
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
