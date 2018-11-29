399c7c7731f3ff0a842f6a07d199866f9925e70b
Ingo Claro <ingo@groupon.com>
2018-11-28 16:55:57 -0800

    update

---
+++ android/env.zsh
@@ -0,0 +1,2 @@
+export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
+export ANDROID_HOME="/Users/ingo/Library/Android/sdk"
--- docker/install.sh
+++ docker/install.sh
@@ -1,4 +1,5 @@
-brew install docker docker-machine
-docker-machine create --driver virtualbox --virtualbox-disk-size 50000 --virtualbox-memory 4096 dev
-sudo route -n add 172.17.0.0/16 $(docker-machine ip dev)
+brew install docker docker-compose
+brew cask install docker
+# docker-machine create --driver virtualbox --virtualbox-disk-size 50000 --virtualbox-memory 4096 dev
+# sudo route -n add 172.17.0.0/16 $(docker-machine ip dev)
 
