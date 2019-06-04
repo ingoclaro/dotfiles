cc3df43184027cf3f5d9bc065d62e197b9f3e9df
Ingo Claro <ingo@groupon.com>
2019-06-04 09:30:51 -0700

    update

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -5,7 +5,7 @@ export ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="ingo"
+ZSH_THEME="spaceship"
 
 # Set to this to use case-sensitive completion
 # CASE_SENSITIVE="true"
@@ -25,6 +25,6 @@ ZSH_THEME="ingo"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(npm autojump sublime git brew bundler capistrano redis-cli vagrant)
+plugins=(npm autojump git brew node osx javascript zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
